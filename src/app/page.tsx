0998e69e3cda34e18157b1c7fbb183efcd53581b
Mubin-Mohammad-Ikhtiar-Khan <mubin.ice.ru@gmail.com>
2023-08-09 04:34:38 +0600

    change home page message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
     <main>
-      <h2>Hello</h2>
+      <h2>Welcome</h2>
     </main>
   )
 }
